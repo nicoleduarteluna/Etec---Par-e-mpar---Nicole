f898110e0108ff539b92332f486c727a281e6542
niluiz <nicoleduarteluna@hotmail.com>
2019-08-21 22:07:55 -0300

    Codificação

--- app/src/main/java/br/com/etechoracio/parimpar/MainActivity.java
+++ app/src/main/java/br/com/etechoracio/parimpar/MainActivity.java
@@ -2,12 +2,32 @@ package br.com.etechoracio.parimpar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.ViewAnimator;
 
 public class MainActivity extends AppCompatActivity {
+    EditText txtnumero;
+    TextView lblResultado2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtnumero = findViewById(R.id.txtNumero);
+        lblResultado2 = findViewById(R.id.lblResultado2) ;
+    }
+
+    public void onProcessar (View v) {
+        int numero = Integer.parseInt(txtnumero.getText().toString());
+
+        if (numero %2 ==0)
+
+    {
+        lblResultado2.setText("PAR");
+    } else {
+            lblResultado2.setText("ÍMPAR");
+        }
     }
 }
